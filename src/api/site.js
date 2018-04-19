2c44fede0ddbbee7566a6e7124016962600282a6
Ben Schwarz <ben@germanforblack.com>
2018-04-19 15:40:27 +1000

    Added Site create function & mutation

--- src/api/site.js
+++ src/api/site.js
@@ -1,6 +1,25 @@
 const gql = require('../utils/api-client')
 const handleError = require('../utils/api-error')
 
+const CREATE_MUTATION = `
+  mutation CreateSite($name:String!, $location: LocationTag!, $pages: [PageInput!]!, $testProfiles:[TestProfileInput!]){
+    createSite(name: $name, pages: $pages, location:$location, testProfiles: $testProfiles) {
+      name
+      slug
+
+      testProfiles {
+        name
+        uuid
+      }
+
+      pages {
+        name
+        uuid
+      }
+    }
+  }
+`
+
 const LIST_QUERY = `
   query {
     organisation {
@@ -13,6 +32,21 @@ const LIST_QUERY = `
   }
 `
 
+const create = async ({ name, location, pages, testProfiles }) => {
+  try {
+    const response = await gql.request(CREATE_MUTATION, {
+      name,
+      location,
+      pages,
+      testProfiles
+    })
+
+    return response.createSite
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
 const list = async () => {
   try {
     const response = await gql.request(LIST_QUERY)
@@ -23,5 +57,6 @@ const list = async () => {
 }
 
 module.exports = {
+  create,
   list
 }
