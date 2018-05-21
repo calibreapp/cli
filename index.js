d4de13f9d60a93ad847cd79d300992960b4e6c07
Ben Schwarz <ben@germanforblack.com>
2018-05-21 12:04:14 +1000

    Use error throwing, fix incorrect path

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const chalk = require('chalk')
-
 if (!process.env.CALIBRE_API_TOKEN) {
-  console.log(
-    chalk.grey(
-      'Please set CALIBRE_API_TOKEN as an environment variable. Create your token via the team API tokens page, or see calibreapp.com/docs/api/tokens for help.'
-    ),
-    chalk.blue(`${process.env.CALIBRE_HOST}/home`)
+  throw new Error(
+    'Please set CALIBRE_API_TOKEN as an environment variable. Create your token via the team API tokens page, or see calibreapp.com/docs/api/tokens for help.'
   )
-  process.exit()
 }
 
 const Site = require('./src/api/site')
 const Snapshot = require('./src/api/snapshot')
 const Test = require('./src/api/test')
-const TestProfile = require('./src/api/test_profile')
+const TestProfile = require('./src/api/test-profile')
 const Agent = require('./src/api/location')
 
 module.exports = {
