a0ae19cb62f1df4fb91dc9d4bf66b544b23fa2bb
Ben Schwarz <ben@germanforblack.com>
2018-07-20 11:24:38 +1000

    Quotes around vars

--- examples/bash/get-agent-ip-address.sh
+++ examples/bash/get-agent-ip-address.sh
@@ -5,4 +5,4 @@ set -e
 AGENTS=$(calibre location-list --json | jq --raw-output '[.[] | select(.tag == "Sydney") | .agents[].ipv4] | join(", ")')
 
 # Print a comma seperated list of ip addresses for a given test location
-echo $AGENTS
+echo "$AGENTS"
