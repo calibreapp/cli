2e76ae9b886f8f7d202194a191137bb5a83f9304
Ben Schwarz <ben@germanforblack.com>
2018-08-26 19:12:49 +1000

    Use number values

--- src/utils/formatters/perf-score.js
+++ src/utils/formatters/perf-score.js
@@ -1,13 +1,7 @@
 const chalk = require('chalk')
 
 module.exports = score => {
-  let grade = 'F'
-  if (score > 20) grade = 'D'
-  if (score > 40) grade = 'C'
-  if (score > 60) grade = 'B'
-  if (score > 80) grade = 'A'
-
-  if (score >= 80) return chalk.green.bold(grade)
-  if (score > 60 && score < 80) return chalk.yellow.bold(grade)
-  if (score <= 60) return chalk.red.bold(grade)
+  if (score >= 80) return chalk.green.bold(score)
+  if (score > 60 && score < 80) return chalk.yellow.bold(score)
+  if (score <= 60) return chalk.red.bold(score)
 }
