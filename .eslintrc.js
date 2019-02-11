ed5c37c1e5f050ac0600f952f4aa491a0dc8c715
Michael Dijkstra <micdijkstra@gmail.com>
2019-02-11 11:33:04 +1100

    Disable rules in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
   rules: {
     'no-console': 0,
     semi: [2, 'never'],
-    quotes: [2, 'single', 'avoid-escape']
+    quotes: [2, 'single', 'avoid-escape'],
+    complexity: 0,
+    duplication: 0
   }
 }
