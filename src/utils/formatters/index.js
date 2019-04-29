356d70ce2be10df221e48eb09b872602fd468fe7
Michael Dijkstra <micdijkstra@gmail.com>
2019-04-29 12:27:03 +1000

    Format function

--- src/utils/formatters/index.js
+++ src/utils/formatters/index.js
@@ -1,5 +1,21 @@
+const filesize = require('./filesize')
+const duration = require('./duration')
+const gradeFormatter = require('./grade-score')
+
+const format = ({ formatter, value }) => {
+  switch (formatter) {
+    case 'humanDuration':
+      return duration(value)
+    case 'fileSize':
+      return filesize(value)
+    default:
+      return value
+  }
+}
+
 module.exports = {
-  filesize: require('./filesize'),
-  duration: require('./duration'),
-  gradeFormatter: require('./grade-score')
+  format,
+  filesize,
+  duration,
+  gradeFormatter
 }
