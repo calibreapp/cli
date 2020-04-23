9f509015aa0d298c62fc1244dee2568e68b7e9fa
Ben Schwarz <ben@calibreapp.com>
2020-04-23 11:14:12 +1000

    Correction: Command and options are swapped

--- src/utils/token.js
+++ src/utils/token.js
@@ -6,7 +6,7 @@ const retrieveToken = () => {
   const token = process.env.CALIBRE_API_TOKEN || config.get('token')
   if (!token) {
     throw new Error(
-      'Please set your Calibre API token by running `calibre set token`\n or set the environment variable CALIBRE_API_TOKEN.\n\n See calibreapp.com/docs/api/tokens for help.'
+      'Please set your Calibre API token by running `calibre token set`\n or set the environment variable CALIBRE_API_TOKEN.\n\n See calibreapp.com/docs/api/tokens for help.'
     )
   }
 
