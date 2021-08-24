0646f8133c36e0e451d06fc9b0808b1c8b1f6895
Jono Gillett <jono@ljx.com.au>
2021-08-24 16:58:32 +1000

    Updated cli option types to specify array on pages and profiles. Updated descriptions on pages, profiles and metrics to include default behaviour when not set.

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -23,17 +23,19 @@ const options = {
     describe: 'The cursor to fetch records after'
   },
   pages: {
+    type: 'array',
     describe:
-      'A space separated list of page uuids to return metrics for. eg: --pages=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --pages for each page'
+      'A space separated list of page uuids to return metrics for (when not set, defaults to all pages). eg: --pages=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --pages for each page'
   },
   profiles: {
+    type: 'array',
     describe:
-      'A space separated list of profile uuids to return metrics for. eg: --profiles=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --profiles for each profile'
+      'A space separated list of profile uuids to return metrics for (when not set, defaults to all test profiles). eg: --profiles=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --profiles for each profile'
   },
   metrics: {
     type: 'array',
     describe:
-      'A space separated list of metrics to return. eg: --metrics=first-meaningful-paint first-interactive or use multiple --metrics flags for each metric'
+      'A space separated list of metrics to return (when not set, defaults to all metrics). eg: --metrics=first-meaningful-paint first-interactive or use multiple --metrics flags for each metric'
   }
 }
 
