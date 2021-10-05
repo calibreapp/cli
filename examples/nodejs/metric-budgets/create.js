00332099c3e2abe04454c9aca239c8ae6aabbee6
Ben Schwarz <ben@calibreapp.com>
2021-10-05 13:20:14 +1100

    Better describe what happens when a budget is created via the API

--- examples/nodejs/metric-budgets/create.js
+++ examples/nodejs/metric-budgets/create.js
@@ -6,6 +6,9 @@ const createMetricBudget = async () => {
   const site = 'calibre' // site slug
   const measurement = 'consistently-interactive' // metric to create budget for
   const value = 5000 // budget value, ms for timing metrics
+  
+  // In this example, the budget is set to one page, for one profile
+  // Omitting `pages` or `profiles` will set the budget on all pages/profiles
   const pages = ['cd7490c0-3d76-4eb3-9761-fccb9214b771'] // uuids of pages
   const profiles = ['58f3667d-4b90-4710-a92f-5e1e0b891818'] // uuids profiles
 
