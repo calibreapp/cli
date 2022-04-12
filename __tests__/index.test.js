cf84968577b17d9ef7d680def1027791b7706a43
Ben Schwarz <ben@calibreapp.com>
2022-04-12 12:58:21 +1000

    Update minimum node

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import * as cli from '../'
 
 describe('Index', () => {
   test('should be able to resolve the same dependencies', () => {
+    console.log(cli)
     expect(cli).toMatchSnapshot()
   })
 })
