3abc9dd9b51be1fc0c86c5bee9d48e387134e9d3
Ben Schwarz <ben@calibreapp.com>
2022-04-22 12:10:08 +1000

    Remove client info headers

--- src/api/graphql.js
+++ src/api/graphql.js
@@ -3,7 +3,6 @@ import pThrottle from 'p-throttle'
 
 import { handleError } from '../utils/api-error.js'
 
-import { getClientInfo } from '../utils/client-info.js'
 import retrieveToken from '../utils/token.js'
 
 const throttle = pThrottle({
@@ -13,15 +12,11 @@ const throttle = pThrottle({
 })
 
 const request = async ({ query, ...variables }) => {
-  const { name, version } = await getClientInfo()
-
   const host = process.env.CALIBRE_HOST || 'https://api.calibreapp.com'
   const endpoint = `${host}/graphql`
   const token = retrieveToken()
   const headers = {
     Accept: 'application/json',
-    'X-Client-Name': name,
-    'X-Client-Version': version,
     'Content-Type': 'application/json',
     Authorization: `Token ${token}`
   }
