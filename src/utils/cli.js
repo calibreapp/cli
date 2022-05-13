e7ec54c43e9bd6e07ad9670f316079e6fea1f878
Karolina Szczur <me@karolinaszczur.com>
2022-05-13 11:25:22 +1000

    ✍️ update site command descriptions

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -1,23 +1,23 @@
 const options = {
   site: {
     demandOption: true,
-    describe: 'The identifying slug of a site',
+    describe: 'A unique slug identifying each Site. You can find it in Site Settings → General or by using the calibre site list command.',
     type: 'string',
     requiresArg: true
   },
-  json: { describe: 'Output in JSON format' },
-  csv: { describe: 'Output in CSV format' },
+  json: { describe: 'Outputs the results of the command in JSON format.' },
+  csv: { describe: 'Outputs the results of the command in CSV format.' },
   from: {
     describe:
-      'The start date to retrieve from in the format Year-Month-Day (when not set, defaults to 7 days ago)'
+      'The start date to retrieve data from in the Year-Month-Day format (default: 7 days ago).'
   },
   to: {
     describe:
-      'The end date to retrieve to in the format Year-Month-Day (when not set, defaults to today)'
+      'The end date to retrieve data from in the Year-Month-Day format (default: today).'
   },
   count: {
     default: 25,
-    describe: 'The number of records to return, maximum: 500'
+    describe: 'The number of items to return (default: 25, maximum: 500).'
   },
   cursor: {
     describe: 'The cursor to fetch records after'
@@ -25,17 +25,17 @@ const options = {
   pages: {
     type: 'array',
     describe:
-      'A space separated list of page uuids to return metrics for (when not set, defaults to all pages). eg: --pages=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --pages for each page'
+      'A list of Page UUIDs to return metrics for. You can separate multiple Pages by a space or use multiple --pages flags.'
   },
   profiles: {
     type: 'array',
     describe:
-      'A space separated list of profile uuids to return metrics for (when not set, defaults to all test profiles). eg: --profiles=4a82662c-67dc-40cd-a461-6afc904260f3 51b3cb71-e534-4d48-842a-d8c9da672f55 or use multiple --profiles for each profile'
+      'A list of Test Profile UUIDs to return metrics for. You can separate multiple Test Profiles by a space or use multiple --profiles flags.'
   },
   metrics: {
     type: 'array',
     describe:
-      'A space separated list of metrics to return (when not set, defaults to all metrics). eg: --metrics=first-meaningful-paint first-interactive or use multiple --metrics flags for each metric'
+      'A list of metric UUIDs to return metrics for. You can separate multiple metrics by a space or use multiple --metrics flags.'
   }
 }
 
