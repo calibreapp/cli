d854ca4f81c9ca00ed6a605bbe53cb36b7f85e5e
Karolina Szczur <me@karolinaszczur.com>
2022-05-13 12:22:25 +1000

    ✍️ update token command description

--- src/cli/token/remove.js
+++ src/cli/token/remove.js
@@ -7,14 +7,14 @@ const config = new Configstore('calibre')
 const main = async () => {
   if (config.get('token')) {
     config.set('token', null)
-    console.log(`${chalk.bold.green(`${logSymbols.success} Token removed!`)}\n`)
+    console.log(`${chalk.bold.green(`${logSymbols.success} API Token removed.`)}\n`)
   } else {
-    console.log(`${chalk.bold.red(`${logSymbols.error} Token is not set!`)}\n`)
+    console.log(`${chalk.bold.red(`${logSymbols.error} API Token is not set.`)}\n`)
   }
 }
 
 const command = 'remove'
-const describe = 'Remove the Calibre API token used for CLI commands'
+const describe = 'Remove the saved API Token used for CLI commands (from ~/.config/configstore/calibre.json).'
 const handler = main
 const builder = {}
 
