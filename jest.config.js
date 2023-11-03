c5d8eb58cdb53f05130d40db70bda5f7f5cb2936
Ben Schwarz <ben@calibreapp.com>
2023-11-03 11:53:02 +1100

    Update jest config, update snaps

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,6 @@ export default {
   testEnvironment: 'node',
   setupFiles: ['./.jest-setup.js'],
   testTimeout: 5000,
-  modulePathIgnorePatterns: ['./dist'],
-  // chalk appears to inline it's dependencies, but jest requires module resolution help
-  // https://github.com/chalk/chalk/blob/main/package.json#L11-L17
-  moduleNameMapper: {
-    '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
-    '#supports-color': 'chalk/source/vendor/supports-color/index.js'
-  }
+  preset: 'ts-jest/presets/default-esm',
+  modulePathIgnorePatterns: ['./dist']
 }
